de45fe758ef8ce618bfc9c5ee15182f62983d7b7
Bruno1776 <brunosantiago7002@gmail.com>
2025-12-12 14:44:21 -0300

    alter links to prod ambient

--- JS/links.js
+++ JS/links.js
@@ -1,11 +1,11 @@
-//const  LoginPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/login.html";
-//const   FormsPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/indexForms.html";
-//const  thankYouPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/thanks.html";
-//const   HomePage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/index.html";
+const  LoginPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/login.html";
+const   FormsPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/indexForms.html";
+const  thankYouPage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/HTML/thanks.html";
+const   HomePage = "https://grupo-novaes.github.io/Trainings_Simple_Platform_No_Videos/index.html";
 
-const  LoginPage = "/HTML/login.html";
-const   FormsPage = "/HTML/indexForms.html";
-const  thankYouPage = "/HTML/thanks.html";
-const   HomePage = "/index.html";
+//const  LoginPage = "/HTML/login.html";
+//const   FormsPage = "/HTML/indexForms.html";
+//const  thankYouPage = "/HTML/thanks.html";
+//const   HomePage = "/index.html";
 
 export default { HomePage, FormsPage,  LoginPage, thankYouPage}
